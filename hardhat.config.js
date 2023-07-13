6d710988eb5224b34164aa4d5ae351c243a2689a
pfed-prog <fedorovn19@gmail.com>
2023-07-13 13:58:50 +0200

    added gas reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  gasReporter: {
+    currency: "CHF",
+    gasPrice: 21,
+    enabled: true,
+  },
 };
